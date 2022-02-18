dc61b192e95d153d06781fecf73aa3c7d192637b
nhanchaukp <ctnhan044@gmail.com>
2022-02-18 11:45:22 +0700

    fix translate

--- src/HideContentInPosts.php
+++ src/HideContentInPosts.php
@@ -32,9 +32,9 @@ class HideContentInPosts extends FormatContent
             $replied = !$serializer->getActor()->isGuest() && in_array($serializer->getActor()->id, $users);
 
             if ($replied)
-                $newHTML = preg_replace('/<login2see>(.*?)<\/login2see>/is', '<div class="login2see"><div class="login2see_title">' . $this->translator->trans('wiseclock-login2see.forum.hidden_content') . '</div>$1</div>', $newHTML);
+                $newHTML = preg_replace('/<login2see>(.*?)<\/login2see>/is', '<div class="login2see"><div class="login2see_title">' . $this->translator->trans('nhanchaukp-login2see.forum.hidden_content') . '</div>$1</div>', $newHTML);
             else
-                $newHTML = preg_replace('/<login2see>(.*?)<\/login2see>/is', '<div class="login2see"><div class="login2see_alert">' . $this->translator->trans('wiseclock-login2see.forum.login_to_see', array('{login}' => '<a class="login2see_login">' . $this->translator->trans('core.forum.discussion_controls.login_button') . '</a>')) . '</div></div>', $newHTML);
+                $newHTML = preg_replace('/<login2see>(.*?)<\/login2see>/is', '<div class="login2see"><div class="login2see_alert">' . $this->translator->trans('nhanchaukp-login2see.forum.login_to_see', array('{login}' => '<a class="login2see_login">' . $this->translator->trans('core.ref.log_in') . '</a>')) . '</div></div>', $newHTML);
 
             $attributes['contentHtml'] = $newHTML;
         }
